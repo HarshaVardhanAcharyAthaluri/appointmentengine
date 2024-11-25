4e56a0a5de409d73344cf2ba586afc1f7e9ffc23
HarshaVardhan <athaluri.harsha@gmail.com>
2024-11-25 11:23:28 +0530

    init repo

---
+++ src/main/java/com/harshaprimelabs/appointmentengine/AppointmentengineApplication.java
@@ -0,0 +1,13 @@
+package com.harshaprimelabs.appointmentengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentengineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harshaprimelabs/appointmentengine/AppointmentengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshaprimelabs.appointmentengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentengineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
